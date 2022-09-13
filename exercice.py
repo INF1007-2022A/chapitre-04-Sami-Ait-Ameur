0b0e868d5dd02d0215a5f1f9bdaba17f92ed37ca
Sami-Ait-Ameur <113061454+Sami-Ait-Ameur@users.noreply.github.com>
2022-09-13 12:16:09 -0400

    Exercice 1 et 2 classe

--- exercice.py
+++ exercice.py
@@ -7,7 +7,7 @@ def is_even_len(string: str) -> bool:
 
 
 def remove_third_char(string: str) -> str:
-    pass
+    return string[:2] + string[3:]
 
 
 def replace_char(string: str, old_char: str, new_char: str) -> str:
